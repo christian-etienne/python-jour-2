018b1d669606cdeda190fcc9e0524460fb80c153
christian etienne <christian-michael.etienne@laplateforme.io>
2024-03-07 15:11:45 +0100

    job finish

---
+++ job 03/main.py
@@ -0,0 +1,13 @@
+
+for x in range (101) :
+    if x in [26 , 37 , 88] :
+     continue       
+    print (x)
+    
+    
+
+  
+
+
+    
+ 
---
+++ job 04/main.py
@@ -0,0 +1,14 @@
+
+def table_multiplication(n):
+ n = int(input("veuillez enter un nbr:"))
+     for x in range(1, 11):
+      print(x, '*', n, '=', x * n)
+      
+  if n > 0:
+     break
+      
+
+#while True:
+   #  n = int(input("veuillez enter un nbr:"))
+    #if n > 0:
+    #    break
